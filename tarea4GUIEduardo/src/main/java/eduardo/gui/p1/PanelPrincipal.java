4e0b5f63d297f8dba8e5bf88ca8693ad32943e51
Eduardo2044 <eduardoorellanapomares@gmail.com>
2022-04-16 20:17:56 +0200

    Implementa la interfaz ActionListener en la clase PanelPrincipal y su metodo (sin funcion aun). En initcomponent se pone a escuchar a todos los botones eventos de ActionListener

--- tarea4GUIEduardo/src/main/java/eduardo/gui/p1/PanelPrincipal.java
+++ tarea4GUIEduardo/src/main/java/eduardo/gui/p1/PanelPrincipal.java
@@ -1,41 +1,50 @@
-
 package eduardo.gui.p1;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
 
-public class PanelPrincipal extends JPanel{
-    
+public class PanelPrincipal extends JPanel implements ActionListener {
+
     // Atributos de la clase (privados)
-	private PanelBotones botonera;
-	private JTextArea areaTexto;
-	private int tipoOperacion;
-    
-	// Constructor
-	public PanelPrincipal(){
-    		initComponents();
-    		tipoOperacion = -1; // No hay operaciones en la calculadora
-	}
+    private PanelBotones botonera;
+    private JTextArea areaTexto;
+    private int tipoOperacion;
+
+    // Constructor
+    public PanelPrincipal() {
+        initComponents();
+        tipoOperacion = -1; // No hay operaciones en la calculadora
+    }
+
+    // Se inicializan los componentes gráficos y se colocan en el panel
+    private void initComponents() {
+        // Creamos el panel de botones
+        botonera = new PanelBotones();
+        // Creamos el área de texto
+        areaTexto = new JTextArea(10, 50);
+        areaTexto.setEditable(false);
+        areaTexto.setBackground(Color.white);
+
+        //Establecemos layout del panel principal
+        this.setLayout(new BorderLayout());
+        // Colocamos la botonera y el área texto
+        this.add(areaTexto, BorderLayout.NORTH);
+        this.add(botonera, BorderLayout.SOUTH);
+
+        for (JButton boton : this.botonera.getgrupoBotones()) {
+            boton.addActionListener(this);
+        }
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
         
-        // Se inicializan los componentes gráficos y se colocan en el panel
-	private void initComponents(){
-    		// Creamos el panel de botones
-    		botonera = new PanelBotones();
-    		// Creamos el área de texto
-    		areaTexto = new JTextArea(10,50);
-    		areaTexto.setEditable(false);
-    		areaTexto.setBackground(Color.white);
-   
-    		//Establecemos layout del panel principal
-    		this.setLayout(new BorderLayout());
-    		// Colocamos la botonera y el área texto
-    		this.add(areaTexto, BorderLayout.NORTH);
-    		this.add(botonera, BorderLayout.SOUTH);
-	 
-	}
+    }
 
-
-    
 }
