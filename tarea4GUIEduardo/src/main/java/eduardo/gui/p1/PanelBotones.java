31c837b5ef90ba5fa57e99938dc89a5878cba735
Eduardo2044 <eduardoorellanapomares@gmail.com>
2022-04-16 19:44:44 +0200

    Crea un array de botones de tamaño 16 para todos los botones de la calculadora

--- tarea4GUIEduardo/src/main/java/eduardo/gui/p1/PanelBotones.java
+++ tarea4GUIEduardo/src/main/java/eduardo/gui/p1/PanelBotones.java
@@ -1,8 +1,13 @@
 
 package eduardo.gui.p1;
 
+import javax.swing.JButton;
+
 public class PanelBotones {
     
-    
+    // Números del 0 al 9 y seis botones más para
+	// suma, resta, multiplicacion, división, resultado y limpiar
+	JButton [] grupoBotones = new JButton[16];
+
     
 }
